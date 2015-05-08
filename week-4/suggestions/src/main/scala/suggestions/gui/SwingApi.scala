0e1912450419e2415ac82edfe57a789e37d927e0
Alfonso Ruzafa <superruzafa@gmail.com>
2015-05-08 20:36:56 +0200

    Solution for the suggestions assignment

--- week-4/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ week-4/suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
-import rx.lang.scala.Observable
+import rx.lang.scala.{Subscription, Observer, Observable}
 
 /** Basic facilities for dealing with Swing-like components.
 *
@@ -51,8 +51,14 @@ trait SwingApi {
       * @param field the text field
       * @return an observable with a stream of text field updates
       */
-    def textValues: Observable[String] = ???
-
+    def textValues: Observable[String] =
+      Observable.create[String] {
+        observer: Observer[String] =>
+          field.subscribe {
+            case _: ValueChanged => observer onNext field.text
+          }
+        Subscription()
+      }
   }
 
   implicit class ButtonOps(button: Button) {
@@ -62,7 +68,14 @@ trait SwingApi {
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
      */
-    def clicks: Observable[Button] = ???
+    def clicks: Observable[Button] =
+      Observable.create {
+        observer: Observer[Button] =>
+          button.subscribe {
+            case _: ButtonClicked => observer.onNext(button)
+          }
+        Subscription()
+      }
   }
 
 }
