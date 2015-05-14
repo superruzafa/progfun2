130c60087c3521d7f9ca8392ec4e947b617da9b1
Alfonso Ruzafa <superruzafa@gmail.com>
2015-05-14 19:25:59 +0200

    Solution for the actorbintree assignment

--- w5-actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
+++ w5-actorbintree/src/main/scala/actorbintree/BinaryTreeSet.scala
@@ -66,15 +66,29 @@ class BinaryTreeSet extends Actor {
 
   // optional
   /** Accepts `Operation` and `GC` messages. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case GC =>
+      val tempRoot = createRoot
+      context.become(garbageCollecting(tempRoot))
+      root ! CopyTo(tempRoot)
+    case msg: Operation => root forward msg
+  }
 
   // optional
   /** Handles messages while garbage collection is performed.
     * `newRoot` is the root of the new binary tree where we want to copy
     * all non-removed elements into.
     */
-  def garbageCollecting(newRoot: ActorRef): Receive = ???
-
+  def garbageCollecting(newRoot: ActorRef): Receive = {
+    case msg: Operation => pendingQueue = pendingQueue :+ msg
+    case GC =>
+    case CopyFinished =>
+      root ! PoisonPill
+      root = newRoot
+      pendingQueue foreach {newRoot ! _}
+      pendingQueue = Queue.empty[Operation]
+      context.become(normal)
+  }
 }
 
 object BinaryTreeNode {
@@ -98,16 +112,72 @@ class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Actor {
 
   // optional
   def receive = normal
+  
+  def position(nElement: Int) = if (nElement > elem) Right else Left
+  
+  def internalOp(nElement: Int, requestor: ActorRef, msg: Operation, elementFound: => Unit, elementNotFound: => Unit) =
+      if (nElement == elem) {
+        elementFound
+        requestor ! OperationFinished(msg.id)
+      } else {
+        val position = if (nElement > elem) Right else Left
+        if (subtrees.contains(position))
+          subtrees(position) forward msg
+        else {
+          elementNotFound
+          requestor ! OperationFinished(msg.id)
+        }
+      }      
 
   // optional
   /** Handles `Operation` messages and `CopyTo` requests. */
-  val normal: Receive = { case _ => ??? }
+  val normal: Receive = {
+    case msg @ Insert(requestor, id, nElement) =>
+      internalOp(nElement, requestor, msg, { removed = false },
+                 {subtrees = subtrees + (position(nElement) -> context.actorOf(BinaryTreeNode.props(nElement, false)))}
+      )
+
+    case msg @ Remove(requestor, id, nElement) =>
+      internalOp(nElement, requestor, msg, { removed = true }, {})
+
+    case msg @ Contains(requestor, id, nElement) =>
+      if (nElement == elem)
+        requestor ! ContainsResult(id, !removed)
+      else {
+        val position = if (nElement > elem) Right else Left
+        if (subtrees.contains(position))
+          subtrees(position) forward msg
+        else
+          requestor ! ContainsResult(id, result = false)
+      }
+
+    case CopyTo(newTree) =>
+      val children = subtrees.values.toSet
+      if (children.isEmpty && removed)
+        context.parent ! CopyFinished
+      else {
+        children.foreach { x => x ! CopyTo(newTree)}
+        if (!removed) newTree ! Insert(self, elem, elem)
+        context.become(copying(children, removed), discardOld = false)
+      }
+  }
 
   // optional
   /** `expected` is the set of ActorRefs whose replies we are waiting for,
     * `insertConfirmed` tracks whether the copy of this node to the new tree has been confirmed.
     */
-  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = ???
+  def copying(expected: Set[ActorRef], insertConfirmed: Boolean): Receive = {
+    case OperationFinished(id) =>
+      if (expected.isEmpty)
+        context.parent ! CopyFinished
+      else
+        context.become(copying(expected, insertConfirmed = true), discardOld = false)
 
-
+    case CopyFinished =>
+      val todo = expected - sender
+      if (todo.isEmpty && insertConfirmed)
+        context.parent ! CopyFinished
+      else
+        context.become(copying(todo, insertConfirmed), discardOld = false)
+  }
 }
